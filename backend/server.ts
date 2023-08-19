438cf08cde9c721e84bdfdd141cbdbc83ed58b0c
Chloe Serapio <caroline.rserapio@gmail.com>
2023-08-19 02:21:38 -0400

    add maps component

--- backend/server.ts
+++ backend/server.ts
@@ -1,13 +1,20 @@
 import express, { Express, Request, Response } from 'express'
+import { LatLngBounds } from 'googlemaps';
 
-const app: Express = express()
+const app = express();
 
-// routes
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server')
-})
+app.use(express.static('build'));
 
-const port = process.env.PORT || 8080
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
-})
+app.get('/api/route', (req, res) => {
+  const routeData = {
+    origin: 'San Francisco, CA',
+    destination: 'Los Angeles, CA',
+    bounds: new LatLngBounds(new google.maps.LatLng(37.7749, -122.4194), new google.maps.LatLng(34.0522, -118.2437)),
+  };
+
+  res.json({ route: routeData });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
--- ecomm/src/pages/NotFound.tsx
+++ ecomm/src/pages/NotFound.tsx
@@ -1,12 +1,54 @@
-import { Flex, Title, Text } from "@mantine/core";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const NotFound = () => {
-  return (
-    <Flex direction="column" sx={{ width: "400px" }}>
-      <Title order={1}>Not found</Title>
-      <Text>That page doesn't exist.</Text>
-    </Flex>
-  );
+const GOOGLE_MAPS_API_KEY = 'AIzaSyCzg1MFFfx7ieuTJKYnTSGe65x4ft33CFw'; 
+
+const Map: React.FC = () => {
+  const [route, setRoute] = useState<{ origin: string; destination: string; bounds: any } | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('/api/route');
+        setRoute(response.data.route);
+      } catch (error) {
+        console.error('Error fetching route:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    if (route) {
+      const directionsService = new window.google.maps.DirectionsService();
+      const directionsRenderer = new window.google.maps.DirectionsRenderer();
+
+      const mapOptions = {
+        center: route.bounds.getCenter(),
+        zoom: 10,
+      };
+
+      const map = new window.google.maps.Map(document.getElementById('map') as HTMLElement, mapOptions);
+      directionsRenderer.setMap(map);
+
+      const request = {
+        origin: route.origin,
+        destination: route.destination,
+        travelMode: 'DRIVING',
+      };
+
+      directionsService.route(request, (result, status) => {
+        if (status === 'OK') {
+          directionsRenderer.setDirections(result);
+        } else {
+          console.error('Error displaying route:', status);
+        }
+      });
+    }
+  }, [route]);
+
+  return <div id="map" style={{ width: '100%', height: '500px' }}></div>;
 };
 
-export default NotFound;
+export default Map;
