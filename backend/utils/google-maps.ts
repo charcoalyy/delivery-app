060b5251b8e88ebfb4f3190e179adf26c173aa24
wx02shi <wx02shi@gmail.com>
2023-08-19 04:12:52 -0400

    update route estimator

--- backend/utils/google-maps.ts
+++ backend/utils/google-maps.ts
@@ -3,16 +3,20 @@ exports.geocode = async (address: string) => {
     `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GOOGLE_MAPS_API_KEY}`
   )
     .then((gres) => gres.json())
-    .then((gres) => gres)
+    .then((gres) => {
+      const best = gres.results[0]
+      const fullAddress = best.formatted_address
+      const location = best.geometry.location
+      return { address: fullAddress, location: location }
+    })
 }
 
-// exports.placeIdCoords = async (placeId: string) => {
-// }
+exports.calculateRoute = async (origin: string, destination: string) => {
+  const originLocation = await exports.geocode(origin)
+  const destinationLocation = await exports.geocode(destination)
 
-exports.calculateRoute = async (
-  originPlaceId: string,
-  destinationPlaceId: string
-) => {
+  console.log(originLocation.location.lat)
+
   return fetch(`https://routes.googleapis.com/directions/v2:computeRoutes`, {
     method: 'POST',
     headers: {
@@ -23,10 +27,20 @@ exports.calculateRoute = async (
     },
     body: JSON.stringify({
       origin: {
-        placeId: originPlaceId,
+        location: {
+          latLng: {
+            latitude: originLocation.location.lat,
+            longitude: originLocation.location.lng,
+          },
+        },
       },
       destination: {
-        placeId: destinationPlaceId,
+        location: {
+          latLng: {
+            latitude: destinationLocation.location.lat,
+            longitude: destinationLocation.location.lng,
+          },
+        },
       },
       travelMode: 'DRIVE',
       routeModifiers: {
