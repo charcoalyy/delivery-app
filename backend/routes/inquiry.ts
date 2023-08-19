957a0277b3abd6f48609d7b7df72e884125edd51
wx02shi <wx02shi@gmail.com>
2023-08-19 03:58:16 -0400

    change to geocode bc significantly cheaper

--- backend/routes/inquiry.ts
+++ backend/routes/inquiry.ts
@@ -1,16 +1,14 @@
 import { Router, Response } from 'express'
-const { addressAutoFill, calculateRoute } = require('../utils/google-maps')
+const { geocode, calculateRoute } = require('../utils/google-maps')
 const router = Router()
 
 router.post('/', (req, res) => {
-  const { originPlaceId, destinationPlaceId } = req.body
-  calculateRoute(originPlaceId, destinationPlaceId).then((gres: Response) =>
-    res.send(gres)
-  )
+  const { origin, destination } = req.body
+  calculateRoute(origin, destination).then((gres: Response) => res.send(gres))
 })
 
-router.get('/address-autofill/:address', async (req, res) => {
-  addressAutoFill(req.params.address).then((gres: Response) => res.send(gres))
+router.get('/geocode/:address', async (req, res) => {
+  geocode(req.params.address).then((gres: Response) => res.send(gres))
 })
 
 export default router
--- backend/utils/google-maps.ts
+++ backend/utils/google-maps.ts
@@ -1,6 +1,6 @@
-exports.addressAutoFill = async (address: string) => {
+exports.geocode = async (address: string) => {
   return fetch(
-    `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${address}&key=${process.env.GOOGLE_MAPS_API_KEY}`
+    `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.GOOGLE_MAPS_API_KEY}`
   )
     .then((gres) => gres.json())
     .then((gres) => gres)
