5f6f94e58e445c653baad8c1926bb9294762a9c0
milo <106563068+charcoalyy@users.noreply.github.com>
2023-08-19 16:04:52 -0400

    prepare ecomm apis

--- ecomm/src/components/forms/Shipping.tsx
+++ ecomm/src/components/forms/Shipping.tsx
@@ -5,30 +5,35 @@ import {
   Select,
   TextInput,
   Title,
-} from "@mantine/core";
-import { useForm } from "@mantine/form";
-import CenterContainer from "src/components/wrappers/CenterContainer";
-import { useMemo } from "react";
-import { formConfig, initialValues, validate } from "./constants";
-import useRequest from "@hooks/useRequest";
-import { postShipping } from "@api/shipping";
+} from '@mantine/core'
+import { useForm } from '@mantine/form'
+import CenterContainer from 'src/components/wrappers/CenterContainer'
+import { useMemo } from 'react'
+import { formConfig, initialValues, validate } from './constants'
+import useRequest from '@hooks/useRequest'
+import { postShipping, shippingInquiry } from '@api/shipping'
 
 const Shipping = ({ setSubmitted }: { setSubmitted: () => void }) => {
   const form = useForm({
     initialValues: initialValues,
     validate: validate,
-  });
+  })
 
-  const { makeRequest } = useRequest({
+  const { makeRequest: postShippingJob } = useRequest({
     request: postShipping,
     requestByDefault: false,
-  });
+  })
+
+  const { makeRequest: getShippingInquiry } = useRequest({
+    request: shippingInquiry,
+    requestByDefault: false,
+  })
 
   const formElements = useMemo(
     () =>
       formConfig.map((config) => {
         switch (config.type) {
-          case "select":
+          case 'select':
             return (
               <Grid.Col span={config.col} key={config.formProps}>
                 <Select
@@ -36,11 +41,11 @@ const Shipping = ({ setSubmitted }: { setSubmitted: () => void }) => {
                   data={config.options || []}
                   placeholder={config.placeholder}
                   label={config.label}
-                  {...form.getInputProps("country")}
+                  {...form.getInputProps('country')}
                 />
               </Grid.Col>
-            );
-          case "text":
+            )
+          case 'text':
           default:
             return (
               <Grid.Col span={config.col} key={config.formProps}>
@@ -50,27 +55,50 @@ const Shipping = ({ setSubmitted }: { setSubmitted: () => void }) => {
                   {...form.getInputProps(config.formProps)}
                 />
               </Grid.Col>
-            );
+            )
         }
       }),
     [form]
-  );
+  )
 
   return (
     <form
       onSubmit={form.onSubmit((values) => {
-        makeRequest(values);
-        setSubmitted();
+        postShippingJob({
+          origin: '65 Dundas St W, Toronto, ON M5G 2C3',
+          destination: `${values.address}, ${values.city}, ${values.province} ${values.postal_code}`,
+        })
+        setSubmitted()
       })}
     >
+      <button
+        onClick={() =>
+          postShippingJob({
+            origin: '65 Dundas St W, Toronto, ON M5G 2C3',
+            destination: '245 Church St, Toronto, ON M5B 1Z4',
+          })
+        }
+      >
+        click me
+      </button>
+      <button
+        onClick={() =>
+          getShippingInquiry({
+            origin: '65 Dundas St W, Toronto, ON M5G 2C3',
+            destination: '245 Church St, Toronto, ON M5B 1Z4',
+          })
+        }
+      >
+        click me2
+      </button>
       <CenterContainer>
         <Grid.Col span={12}>
           <SegmentedControl
             fullWidth
             data={[
-              { value: "order", label: "Order Details", disabled: true },
-              { value: "payment", label: "Payment", disabled: true },
-              { value: "shipping", label: "Shipping" },
+              { value: 'order', label: 'Order Details', disabled: true },
+              { value: 'payment', label: 'Payment', disabled: true },
+              { value: 'shipping', label: 'Shipping' },
             ]}
           />
         </Grid.Col>
@@ -82,7 +110,7 @@ const Shipping = ({ setSubmitted }: { setSubmitted: () => void }) => {
         {formElements}
         <Grid.Col
           span={12}
-          sx={{ display: "flex", justifyContent: "flex-end" }}
+          sx={{ display: 'flex', justifyContent: 'flex-end' }}
         >
           <Button color="violet" type="submit">
             Submit
@@ -90,7 +118,7 @@ const Shipping = ({ setSubmitted }: { setSubmitted: () => void }) => {
         </Grid.Col>
       </CenterContainer>
     </form>
-  );
-};
+  )
+}
 
-export default Shipping;
+export default Shipping
